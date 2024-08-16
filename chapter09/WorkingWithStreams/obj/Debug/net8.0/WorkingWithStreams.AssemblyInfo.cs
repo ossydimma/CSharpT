85949463f3d2c893fce806d222587e8f2c6c5666
Osita <chrisjerry070@gmail.com>
2024-08-16 20:40:12 +0400

    learnt about Encoding and decoding

--- chapter09/WorkingWithStreams/obj/Debug/net8.0/WorkingWithStreams.AssemblyInfo.cs
+++ chapter09/WorkingWithStreams/obj/Debug/net8.0/WorkingWithStreams.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkingWithStreams")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443cfef869c723d3f9ef079a973c25613127d86b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0825afbbfd8fba877d2a2f6b44910c3cca802f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkingWithStreams")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkingWithStreams")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
