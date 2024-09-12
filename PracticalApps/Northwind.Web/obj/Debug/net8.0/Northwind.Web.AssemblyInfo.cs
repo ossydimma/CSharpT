07d085f220bdb7e5e85e1527ee937d8a0555b4f3
Osita <chrisjerry070@gmail.com>
2024-09-12 20:00:04 +0400

    exploring asp.net core razor pages created razor page called Suppliers that comes with two file cshtml for the html codes and cshtml.cs for the executable c# code this method is called code-behind files helps organize codes

--- PracticalApps/Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
+++ PracticalApps/Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c4619a9abc42982c9d2d4c59dd4798bd1d1e98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824c12db3ebe6a3cc7f7ba70b986562531735fda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
